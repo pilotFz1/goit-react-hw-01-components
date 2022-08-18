194aa3b94511f1950009197a8b39ede49213abfd
Roma Romanenko <romanenko.roma92@gmail.com>
2022-08-18 13:27:55 +0300

    completed 1 task

---
+++ src/components/UserCard/UserCard.jsx
@@ -0,0 +1,48 @@
+import css from '../UserCard/UserCard.module.css';
+import PropTypes from 'prop-types';
+
+export const UserCard = ({ avatar, username, tag, location, stats }) => {
+  return (
+    <div className={css.title}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>
+            Followers <br />
+          </span>
+          <span className={css.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>
+            Views <br />
+          </span>
+          <span className={css.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={css.label}>
+            Likes <br />
+          </span>
+          <span className={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+UserCard.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
